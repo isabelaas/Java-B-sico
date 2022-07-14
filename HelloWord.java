947a9e997fa15059a64e220f457e3d631b411f74
isabelaas <78918353+isabelaas@users.noreply.github.com>
2022-07-13 21:41:06 -0300

    Add files via upload
    Atividades elaboradas no editor de texto e compilado na linha de comando linux Ubuntu

---
+++ HelloWord.java
@@ -0,0 +1,16 @@
+class HelloWord {
+
+	public static void main (String[] args){
+	
+		System.out.println("Hello Word!");
+	}		
+}
+
+//class = Declaração de Classe e nome da classe
+//Método = public static até a primeira chave
+//Modificadores de Acesso do Método = public static
+//Tipo de retorno do método = void (não retorna nada)
+//Tipo do Método = main
+//Argumentos/Parâmetros = Strings[] args
+//Nosso código = System.out.println("Hello Word!");
+//Blocos estão entre {}
---
+++ OiBela.java
@@ -0,0 +1,7 @@
+class OiBela{
+
+	public static void main (String[] args){
+
+		System.out.println("Oi Bela");
+	}
+}
